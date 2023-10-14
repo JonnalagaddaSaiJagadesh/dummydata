13f594003ebe6440022f1adbfdf577640f28c652
jaggu2362 <saijagadesh16@gmail.com>
2023-10-15 00:00:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import { detail } from './components/details';
+import { Detail } from './components/details';
 function App() {
   return (
     <div className="App">
